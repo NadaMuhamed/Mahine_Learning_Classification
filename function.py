517437dcdbfb178d6c0598e61246afe1542bd371
NadaMuhamed <77587501+NadaMuhamed@users.noreply.github.com>
2022-04-30 22:47:24 +0200

    svm

---
+++ function.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+from datetime import datetime
+from datetime import timedelta
+from sklearn.preprocessing import *
+import re
+import time
