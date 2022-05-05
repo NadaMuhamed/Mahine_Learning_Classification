754f15d64fbdbbd21c91f23279f69ae2819f042f
NadaMuhamed <77587501+NadaMuhamed@users.noreply.github.com>
2022-05-05 02:52:27 +0200

    preprocessing

--- function.py
+++ function.py
@@ -12,3 +12,75 @@ from datetime import timedelta
 from sklearn.preprocessing import *
 import re
 import time
+import ast
+import math
+from datetime import timedelta, datetime
+from sklearn.preprocessing import LabelEncoder
+import re
+
+
+
+def time_taken_to_seconds(X):
+    UNITS = {"s": "seconds", "m": "minutes", "h": "hours", "d": "days", "w": "weeks"}
+    return [int(timedelta(**{
+        UNITS.get(m.group('unit').lower(), 'seconds'): float(m.group('val'))
+        for m in re.finditer(r'(?P<val>\d+(\.\d+)?)(?P<unit>[smhdw]?)', s, flags=re.I)
+    }).total_seconds()) for s in X['time_taken']]
+
+
+
+def Feature_Encoder(X,cols):
+    for c in cols:
+        lbl = LabelEncoder()
+        lbl.fit(list(X[c].values))
+        X[c] = lbl.transform(list(X[c].values))
+    return X
+
+
+def dictionary_to_columns(X, colmn):
+    temp = X[colmn].values
+    X.drop(colmn, inplace=True, axis=1)
+    s = [ast.literal_eval(d)['source'] for d in temp]
+    d = [ast.literal_eval(d)['destination'] for d in temp]
+    X['source'] = s
+    X['destination'] = d
+    return X
+
+
+def Date_Converter(X):
+    datalist = [datetime.timestamp(datetime.strptime(d, '%d/%m/%Y')) for d in [t.replace('-', '/') for t in X['date'].values]]
+    X['date'] = datalist
+    temp=[int(str(p)[:-4]) for p in X['date'].values]
+    X['date']=temp
+
+    return X
+
+
+def Stop_Feature(column):
+    values = ["1stop", "nonstop", "2stop"]
+    spec_chars = ["!", '"', "#", "%", "&", "'", "(", ")",
+                  "*", "+", ",", "-", ".", "/", ":", ";", "<",
+                  "=", ">", "?", "@", "[", "\\", "]", "^", "_",
+                  "`", "{", "|", "}", "~", "–"]
+    for char in spec_chars:
+        column = column.str.replace(char, '', regex=True)
+
+    column = column.replace(values[0], 1, regex=True)
+    column = column.replace(values[1], 0, regex=True)
+    column = column.replace(values[2], 2, regex=True)
+    return column
+
+
+
+def converttomin(x):
+    x=x.str.split(':').apply(lambda x: int(x[0]) * 60*60 + int(x[1])*60)
+    return x
+
+
+def handel_price(Y):
+    return [ int(f) for f in[ t.replace(',','') for t in Y]]
+
+
+
+
+
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from sklearn import datasets
-from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from sklearn import linear_model
 from sklearn import metrics
@@ -11,6 +10,37 @@ from sklearn.metrics import r2_score
 from datetime import datetime
 from datetime import timedelta
 from sklearn.preprocessing import *
-from function import *
 import re
 import time
+from function import *
+##########################################################
+data = pd.read_csv('airline-price-prediction.csv')
+data.dropna(how='any', inplace=True)
+#data['date'] = data['date'].replace([''],'13-03-2022')
+##########################################################
+X = data.iloc[:, 0:10]
+Y = handel_price(data['price'])
+X = dictionary_to_columns(X, 'route')
+cols = ('airline', 'ch_code', 'type', 'source', 'destination','TicketCategory')
+X = Feature_Encoder(X, cols)
+X = Date_Converter(X)
+X['time_taken'] = time_taken_to_seconds(X)
+X['stop'] = Stop_Feature(X['stop'])
+X['dep_time'] = converttomin(X['dep_time'])
+X['arr_time'] = converttomin(X['arr_time'])
+airline = X
+airline['price'] = Y
+
+###########################"Model 1"###############################
+print("\n  Model 1  \n")
+
+###########################"Model 2"###############################
+print("\n  Model 2  \n")
+
+###########################"Model 3"###############################
+print("\n  Model 3  \n")
+
+###########################"Model 4"###############################
+print("\n  Model 4  \n")
+
+####################################################################################################################
