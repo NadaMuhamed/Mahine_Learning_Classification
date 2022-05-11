054178977b56d519dc028e9b992125ef01d766bb
NadaMuhamed <77587501+NadaMuhamed@users.noreply.github.com>
2022-05-11 13:44:21 +0200

    Model3 Adaboost

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import statistics
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from nltk import DecisionTreeClassifier
 from sklearn import datasets
 import matplotlib.pyplot as plt
 from sklearn import linear_model
@@ -15,7 +16,7 @@ import re
 import time
 from function import *
 from statistics import mean
-from fractions import Fraction as fr
+from fractions import Fraction
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.preprocessing import StandardScaler
@@ -24,6 +25,14 @@ from sklearn.svm import LinearSVC
 from sklearn.svm import SVC
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.datasets import make_classification
+from sklearn.model_selection import cross_val_score
+from sklearn.datasets import make_blobs
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.datasets import make_gaussian_quantiles
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import accuracy_score
 ##########################################################
 data = pd.read_csv('airline-price-classification.csv')
 #data.dropna(how='any', inplace=True)
@@ -55,11 +64,12 @@ X['type'].fillna(mean(X['type']), inplace = True)
 X['source'].fillna(mean(X['source']), inplace = True)
 X['destination'].fillna(mean(X['destination']), inplace = True)
 Y.fillna(mean(Y), inplace = True)
-X['TicketCategory']=Y
+
 airline = X
 airline['TicketCategory'] = Y
 ###########################"Model 1"###############################
 print("\n  Model 1  \n")
+
 x_train1, x_test1, y_train1, y_test1 =train_test_split(X, Y, test_size=0.3, random_state=0,shuffle=True)
 scaler = StandardScaler()
 x_train1 = scaler.fit_transform(x_train1)
@@ -77,7 +87,6 @@ print("Training Model")
 print("regression score",model1.score(x_train1, y_train1))
 print("Testing Model")
 print("regression score",model1.score(x_test1, y_test1))
-print('Mean Square Error', metrics.mean_squared_error(y_test1, y_pred1))
 
 confusion_matrix(y_test1, y_pred1)
 cm = confusion_matrix(y_test1, y_pred1)
@@ -103,14 +112,60 @@ fig.tight_layout()
 
 ###########################"Model 2"###############################
 print("\n  Model 2  \n")
+
 x_train2, x_test2, y_train2, y_test2 =train_test_split(X, Y, test_size=0.3, random_state=0,shuffle=True)
-svm = svm.SVC(C=100.0, kernel='poly', degree=10)
+print(x_train2.shape)
+svm = svm.SVC(C=100.0, kernel='poly', degree=2)
+print('started training')
 svm.fit(x_train2,y_train2)
 print("training Model")
-print("regression score",svm.score(x_test2,y_test2))
+print("score",svm.score(x_train2,y_train2))
 print("testing Model")
-print("regression score",svm.score(x_test2,y_test2))
+print("score",svm.score(x_test2,y_test2))
 print("Report Model 2 SVM")
 y_pred2 = svm.predict(x_test2)
 print(classification_report(y_test2, y_pred2))
 ###########################"Model 3"###############################
+print("\n  Model 3  \n")
+
+x_train3, x_test3, y_train3, y_test3 =train_test_split(X, Y, test_size=0.3, random_state=0,shuffle=True)
+"""
+x, y = make_classification(n_samples=1000, n_features=4,
+                           n_informative=2, n_redundant=0,
+                           random_state=0, shuffle=False)
+
+clf = AdaBoostClassifier(n_estimators=100, random_state=0)
+
+clf=AdaBoostClassifier(DecisionTreeClassifier(max_depth=1),
+                       algorithm="SAMME", n_estimation=200)
+
+clf=sklearn.ensemble.AdaBoostClassifier(base_estimator=None, *,
+                                        n_estimators=50,
+                                        learning_rate=1.0,
+                                        algorithm='SAMME.R',
+                                        random_state=None)
+                                   
+clf = AdaBoostClassifier(DecisionTreeClassifier(max_depth=1),
+                         algorithm='SAMME',
+                         n_estimators=100,
+                         random_state=0)
+                         
+clf = DecisionTreeClassifier(max_depth=None,
+                             min_samples_split=2,
+                             random_state=0)                         
+                         
+"""
+
+clf=AdaBoostClassifier(DecisionTreeClassifier(max_depth=1),
+                       algorithm="SAMME", n_estimators=200)
+clf.fit(x_train3, y_train3)
+AdaBoostClassifier(n_estimators=100, random_state=0)
+print("training Model")
+print("regression score",clf.score(x_train3, y_train3))
+print("testing Model")
+print("regression score",clf.score(x_test3, y_test3))
+print("Report Model 2 SVM")
+y_pred3 = clf.predict(x_test3)
+print(classification_report(y_test3, y_pred3))
+
+####################################################################################################################
